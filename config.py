64f27a44713b050af37e29b7e8f3bc458bee0097
yuriri096@gmail.com <yuriri096@gmail.com>
2024-03-01 23:56:53 +0300

    '1'

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = '7114755702:AAF7PO4I-HC0BoJpn6cn8Dz9jCs3-pynVdk'
-API_URL = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN = '--'
+API_URL = '--'
 ADMIN_ID = 'kittenmeme_bot'
 START_TEXT = 'Привет! Этот бот может отправить тебе 10 картинок котиков или создать свою смешную картинку.' \
              'Чтобы получить 10 котиков отправь /get_cat ' \
