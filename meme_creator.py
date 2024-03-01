5c1b12c6e2eded926ba46b976897fd698bf053cd
yuriri096@gmail.com <yuriri096@gmail.com>
2024-03-01 23:35:57 +0300

    '1'

--- meme_creator.py
+++ meme_creator.py
@@ -8,7 +8,7 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 from aiohttp import ClientSession, ClientError
 from aiogram import Dispatcher
-from handlers import router, bot
+from handlers import *
 
 
 async def get_cat_images(count: int = 10) -> Any:
@@ -36,25 +36,48 @@ async def create_meme(image_url: str, text0: str, text1: str) -> Any:
 
         draw: ImageDraw = ImageDraw.Draw(image)
 
-        font_path: str = os.path.join(os.getcwd(), "arial.ttf")
+        font_path = os.path.join(os.getcwd(), "arial.ttf")
 
         font: ImageFont = ImageFont.truetype(font_path, size=40)
+        words_limit = 4
+        lst0 = text0.split()
+        lst1 = text1.split()
+        segments0 = []
+        segments1 = []
+        segments0.append('')
+        segments1.append('')
 
+        for i, t0 in enumerate(lst0):
+            segments0[-1] += t0 + ' '
+            if i % words_limit == 0 and len(lst0) > 4:
+                segments0.append('')
+
+        for i, t1 in enumerate(lst1):
+            segments1[-1] += t1 + ' '
+            if i % words_limit == 0 and len(lst1) > 4:
+                segments1.append('')
+        max0 = len(max(segments0, key=lambda x: len(x)))
+        max1 = len(max(segments1, key=lambda x: len(x)))
+
+        segments0 = ['  ' * ((max0 - len(s)) // 2 - 1) + s for s in segments0]
+        segments1 = ['  ' * ((max1 - len(s)) // 2 - 1) + s for s in segments1]
+        text0 = '\n'.join(segments0)
+        text1 = '\n'.join(segments1)
         text0_bbox = draw.textbbox((0, 0), text0, font=font)
         text0_width, text0_height = text0_bbox[2] - text0_bbox[0], text0_bbox[3] - text0_bbox[1]
 
         text1_bbox = draw.textbbox((0, 0), text1, font=font)
         text1_width, text1_height = text1_bbox[2] - text1_bbox[0], text1_bbox[3] - text1_bbox[1]
 
-        text0_position: Tuple[int, int] = ((image.width - text0_width) // 3, 0)
-        text1_position: Tuple[int, int] = ((image.width - text1_width) // 3, image.height - text1_height - 16)
+        text0_position: Tuple[int, int] = ((image.width - text0_width) // 2, 0)
+        text1_position: Tuple[int, int] = ((image.width - text1_width) // 2, image.height - text1_height - 16)
 
         draw.text(text0_position, text0, font=font, fill='white')
         draw.text(text1_position, text1, font=font, fill='white')
 
         image = image.convert("RGB")
-        random_filename: str = str(random.randint(1, 10000))
-        image.save(os.path.join(os.getcwd(),  f"{random_filename}.jpeg"), format='JPEG', encoding='utf-8')
+        random_filename = str(random.randint(1, 10000))
+        image.save(os.path.join(os.getcwd(), f"{random_filename}.jpeg"), format='JPEG', encoding='utf-8')
 
         return f"{random_filename}.jpeg"
 
