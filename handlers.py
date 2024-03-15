e9137ae8fdc275b2067c6cf26e33a06df3ff3789
yuriri096@gmail.com <yuriri096@gmail.com>
2024-03-15 19:15:58 +0300

    'black'

--- handlers.py
+++ handlers.py
@@ -14,11 +14,13 @@ bot = Bot(token=BOT_TOKEN)
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer_sticker(sticker="CAACAgIAAxkBAAMZZeEEBA9hgV8nuK-zIT1xbwGsSgoAAs0rAALEkbBIcYhgxom4IKM0BA")
+    await message.answer_sticker(
+        sticker="CAACAgIAAxkBAAMZZeEEBA9hgV8nuK-zIT1xbwGsSgoAAs0rAALEkbBIcYhgxom4IKM0BA"
+    )
     await message.answer(text=START_TEXT)
 
 
-@router.message(Command('get_cat'))
+@router.message(Command("get_cat"))
 async def send_cat_photos(message: types.Message):
     image_urls: Any = await get_cat_images(count=10)
     for url in image_urls:
@@ -46,13 +48,13 @@ def split_text_by_words(input_string):
         first_part = words[0]
         second_part = words[1]
     else:
-        first_part = ' '.join(words[:split_index + 1])
-        second_part = ' '.join(words[split_index + 1:])
+        first_part = " ".join(words[: split_index + 1])
+        second_part = " ".join(words[split_index + 1 :])
 
     return first_part, second_part
 
 
-@router.message(Command('meme'))
+@router.message(Command("meme"))
 async def create_and_send_meme(message: Message) -> None:
     text_args: str = message.text[5:]
     text_input: str = text_args if len(text_args) > 0 else "Прикрепи текст"
@@ -62,10 +64,10 @@ async def create_and_send_meme(message: Message) -> None:
     image_url: str = await get_cat_images(1)
 
     name: str = await create_meme(image_url[0], text0, text1)
-    photo: FSInputFile = FSInputFile(name, 'rb')
+    photo: FSInputFile = FSInputFile(name, "rb")
 
     if os.path.getsize(name) != 0:
-        await message.answer(text='Твое изображение готово')
+        await message.answer(text="Твое изображение готово")
         await bot.send_photo(message.chat.id, photo)
         os.remove(name)
     else:
--- meme_creator.py
+++ meme_creator.py
@@ -7,17 +7,16 @@ import io
 import random
 from typing import Tuple, Any, List
 
+
 async def get_cat_images(count: int = 10) -> Any:
-    params: dict = {
-        'limit': count
-    }
+    params: dict = {"limit": count}
 
     try:
         async with ClientSession() as session:
             async with session.get(API_URL, params=params) as response:
                 response.raise_for_status()
                 data: list = await response.json()
-                image_urls: List[str] = [cat['url'] for cat in data]
+                image_urls: List[str] = [cat["url"] for cat in data]
                 return image_urls
     except ClientError as e:
         print(f"Error fetching cat images: {e}")
@@ -40,40 +39,53 @@ async def create_meme(image_url: str, text0: str, text1: str) -> Any:
         lst1 = text1.split()
         segments0 = []
         segments1 = []
-        segments0.append('')
-        segments1.append('')
+        segments0.append("")
+        segments1.append("")
 
         for i, t0 in enumerate(lst0):
-            segments0[-1] += t0 + ' '
+            segments0[-1] += t0 + " "
             if i % words_limit == 0 and len(lst0) > 4:
-                segments0.append('')
+                segments0.append("")
 
         for i, t1 in enumerate(lst1):
-            segments1[-1] += t1 + ' '
+            segments1[-1] += t1 + " "
             if i % words_limit == 0 and len(lst1) > 4:
-                segments1.append('')
+                segments1.append("")
         max0 = len(max(segments0, key=lambda x: len(x)))
         max1 = len(max(segments1, key=lambda x: len(x)))
 
-        segments0 = ['  ' * ((max0 - len(s)) // 2 - 1) + s for s in segments0]
-        segments1 = ['  ' * ((max1 - len(s)) // 2 - 1) + s for s in segments1]
-        text0 = '\n'.join(segments0)
-        text1 = '\n'.join(segments1)
+        segments0 = ["  " * ((max0 - len(s)) // 2 - 1) + s for s in segments0]
+        segments1 = ["  " * ((max1 - len(s)) // 2 - 1) + s for s in segments1]
+        text0 = "\n".join(segments0)
+        text1 = "\n".join(segments1)
         text0_bbox = draw.textbbox((0, 0), text0, font=font)
-        text0_width, text0_height = text0_bbox[2] - text0_bbox[0], text0_bbox[3] - text0_bbox[1]
+        text0_width, text0_height = (
+            text0_bbox[2] - text0_bbox[0],
+            text0_bbox[3] - text0_bbox[1],
+        )
 
         text1_bbox = draw.textbbox((0, 0), text1, font=font)
-        text1_width, text1_height = text1_bbox[2] - text1_bbox[0], text1_bbox[3] - text1_bbox[1]
+        text1_width, text1_height = (
+            text1_bbox[2] - text1_bbox[0],
+            text1_bbox[3] - text1_bbox[1],
+        )
 
         text0_position: Tuple[int, int] = ((image.width - text0_width) // 2, 0)
-        text1_position: Tuple[int, int] = ((image.width - text1_width) // 2, image.height - text1_height - 16)
+        text1_position: Tuple[int, int] = (
+            (image.width - text1_width) // 2,
+            image.height - text1_height - 16,
+        )
 
-        draw.text(text0_position, text0, font=font, fill='white')
-        draw.text(text1_position, text1, font=font, fill='white')
+        draw.text(text0_position, text0, font=font, fill="white")
+        draw.text(text1_position, text1, font=font, fill="white")
 
         image = image.convert("RGB")
         random_filename = str(random.randint(1, 10000))
-        image.save(os.path.join(os.getcwd(), f"{random_filename}.jpeg"), format='JPEG', encoding='utf-8')
+        image.save(
+            os.path.join(os.getcwd(), f"{random_filename}.jpeg"),
+            format="JPEG",
+            encoding="utf-8",
+        )
 
         return f"{random_filename}.jpeg"
 
@@ -90,4 +102,3 @@ async def download_image(url: str) -> bytes:
     except Exception as e:
         print(f"Error downloading image: {e}")
         return None
-
